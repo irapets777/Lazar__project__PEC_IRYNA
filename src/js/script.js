0302acb4f2d5679b093d648cfcb3773e391c595c
irapets777 <irapets777@gmail.com>
2022-11-30 16:10:16 +0200

    lazar__end

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+// let slider = $(document).ready(function () {
+//   $('.your-class').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     dots: true,
+//     infinite: true
+//   });
+// });
+'use strict';
+const humb = document.querySelector('#burger');
+const humbBody = document.querySelector('#burger-menu');
+
+
+humb.addEventListener('click', humbHandler);
+function humbHandler(e) {
+   e.preventDefault();
+   humbBody.classList.toggle("open");
+   humb.classList.toggle('active');
+}
 
